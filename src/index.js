44075a1d068fd02baedef15fffc835f45d83f384
anastasiaval <36269769+anastasiaval@users.noreply.github.com>
2020-01-26 18:13:21 +0300

    features

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import './fonts/font-awesome-4.7.0/scss/font-awesome.scss';
 
 import $ from "jquery";
 
+$('.header__nav').on('click','a', function (event) {
+    event.preventDefault();
+    let id  = $(this).attr('href');
+    let top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 800);
+});
+
