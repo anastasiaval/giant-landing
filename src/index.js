d2b1d80bbef612a57b5dec9e7d5750035dc5517a
anastasiaval <36269769+anastasiaval@users.noreply.github.com>
2020-01-27 19:49:22 +0300

    screenshots section

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import './css/main.scss';
 import './css/fonts.scss';
 import './fonts/font-awesome-4.7.0/scss/font-awesome.scss';
+import './js/jquery.waterwheelCarousel';
+import 'slick-carousel/slick/slick.scss';
+import 'slick-carousel/slick/slick-theme.scss';
 
 import $ from "jquery";
+
+require('slick-carousel');
 
 $('.header__nav').on('click','a', function (event) {
     event.preventDefault();
@@ -10,4 +15,14 @@ $('.header__nav').on('click','a', function (event) {
     let top = $(id).offset().top;
     $('body,html').animate({scrollTop: top}, 800);
 });
+
+const carousel = $('.screenshots__slider').waterwheelCarousel({
+    sizeMultiplier: 1,
+    opacityMultiplier: 1,
+    separation: 226
+});
+
+$('.screenshots__arrow_left').on('click', () => carousel.prev());
+
+$('.screenshots__arrow_right').on('click', () => carousel.next());
 
